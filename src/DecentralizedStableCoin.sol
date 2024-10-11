7d434779a843e552453463794d4e5849fb0e43c1
peterberekvolgyi <berekvolgyipeter@gmail.com>
2024-10-11 18:55:12 +0200

    DSCEngine skeleton

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -5,6 +5,13 @@ import {ERC20Burnable, ERC20} from "openzeppelin/token/ERC20/extensions/ERC20Bur
 import {Ownable} from "openzeppelin/access/Ownable.sol";
 
 /* 
+ * @title DecentralizedStableCoin
+ * 
+ * Collateral: Exogenous
+ * Minting (Stability Mechanism): Decentralized (Algorithmic)
+ * Value (Relative Stability): Anchored (Pegged to USD)
+ * Collateral Type: Crypto
+ *
  * This is the contract meant to be owned by DSCEngine.
  * It is a ERC20 token that can be minted and burned by the DSCEngine smart contract.
  */
@@ -13,7 +20,7 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
     error DecentralizedStableCoin__NotZeroAddress();
 
-    constructor(address _owner) ERC20("DecentralizedStableCoin", "DSC") Ownable(_owner) {}
+    constructor(address initialOwner) ERC20("DecentralizedStableCoin", "DSC") Ownable(initialOwner) {}
 
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
